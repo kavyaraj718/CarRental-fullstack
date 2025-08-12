9eb8eea1e814dd1d6dedfe014cdbb739cde30a8f
kavyaraj718 <singhkavyaraj@gmail.com>
2025-08-12 17:56:17 +0530

    edit done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import CarDetails from './pages/CarDetails';
