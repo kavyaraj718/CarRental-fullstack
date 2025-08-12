506b3d9584c12034a537358632f19dec2c862ef9
kavyaraj718 <singhkavyaraj@gmail.com>
2025-08-12 18:34:27 +0530

    edit

--- client/src/pages/Owner/Layout.jsx
+++ client/src/pages/Owner/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Sidebar from '../../components/owner/Sidebar'
 import { Outlet } from 'react-router-dom'
 import NavbarOwner from '../../components/owner/NavbarOwner'
-import { useAppContext } from '../../context/appContext'
+import { useAppContext } from '../../context/AppContext'
 import { useEffect } from 'react'
 
 const Layout = () => {
